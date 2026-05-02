093c4680c8b653fe6d6797eb8d3e47cc42a80b5f
Oshadha Pathiraja <157488657+oshadha2k01@users.noreply.github.com>
2026-05-02 11:13:42 +0530

    fix: resolve the app error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ val googleMapsApiKey =
         ?: ""
 
 android {
-    namespace = "com.example.frontend"
+    namespace = "com.example.DriverApp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -36,7 +36,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.frontend"
+        applicationId = "com.example.DriverApp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
